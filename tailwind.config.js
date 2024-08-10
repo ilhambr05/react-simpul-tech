40cf66bdb5064ba5d259c67d44b46475226a34c2
Ilham Burhanuddin <ilham.r@wistkey.dev>
2024-08-10 07:20:13 +0700

    add color configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ export default {
         'lato': ['"Lato"', ...defaultTheme.fontFamily.sans],
       },
     },
+    colors: {
+      "primary-blue": "#2F80ED",
+      "primary-dark": "#333333",
+      "primary-dark-grey": "#4F4F4F",
+      "primary-grey": "#828282",
+      "primary-light-grey": "#E0E0E0",
+      "indicator-orange": "#F8B76B",
+      "indicator-purple": "#8785FF"
+    }
   },
   plugins: [],
 }

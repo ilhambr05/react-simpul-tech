2cc6e98ab742663a65c2a1606d376dcb264a4a0b
Ilham Burhanuddin <ilham.r@wistkey.dev>
2024-08-12 06:31:42 +0700

    apply loader on chat to mock API call

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,17 +1,15 @@
 import ChatSummary from "./ChatSummary";
-import dummyChatSummary from "../dummy/chat";
 import { useState } from "react";
 import ChatDetail from "./ChatDetail";
 
 function Chat() {
-    const chatSummary = dummyChatSummary;
     const [chatDetailId, setChatDetailId] = useState(0);
 
     return (
         <>
             {chatDetailId
                 ? <ChatDetail chatID={chatDetailId} setChatDetailId={setChatDetailId} />
-                : <ChatSummary chatSummary={chatSummary} setChatDetailId={setChatDetailId} />
+                : <ChatSummary setChatDetailId={setChatDetailId} />
             }
         </>
     )
--- src/components/ChatSummary.jsx
+++ src/components/ChatSummary.jsx
@@ -1,48 +1,68 @@
+import { useEffect, useState } from "react";
+import Loader from "./UI/Loader";
 import SearchBar from "./UI/SearchBar";
 
-function ChatSummary({ chatSummary, setChatDetailId }) {
+import dummyChatSummary from "../dummy/chat";
+
+function ChatSummary({ setChatDetailId }) {
+    const [chatSummary, setChatSummary] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        setTimeout(() => {
+            setChatSummary(dummyChatSummary);
+            setIsLoading(false);
+        }, 1000);
+    }, []);
+
     return (
         <>
             <SearchBar />
-            <div className="overflow-y-auto">
-                {
-                    chatSummary.map((summary, index) => {
-                        return (
-                            <div key={index} className="flex flex-row py-[22px] text-[14px] border-b-primary-dark border-b-[1px] border-solid 
+            {
+                isLoading
+                    ? <Loader>Loading Chat</Loader>
+                    :
+                    <div className="overflow-y-auto">
+                        {
+                            chatSummary.map((summary, index) => {
+                                return (
+                                    <div key={index} className="flex flex-row py-[22px] text-[14px] border-b-primary-dark border-b-[1px] border-solid 
                             last:border-none cursor-pointer hover:bg-[#D2D2D22B]"
-                                onClick={() => setChatDetailId(summary.id)}>
-                                <div className="flex flex-row min-w-[50px] overflow-hidden mr-[10px]">
-                                    {
-                                        summary.participants.map((user, index) => {
-                                            return (
-                                                <div key={user.name + index} className="w-[15px] overflow-visible" title={user.name}>
-                                                    <div className={`flex items-center justify-center h-[30px] w-[30px] rounded-full ${user.styleClass || "bg-[#ffffff]"}`}>
-                                                        <img className="w-[12px] h-[12px]" src={user.icon}></img>
-                                                    </div>
-                                                </div>
-                                            )
-                                        })
-                                    }
-                                </div>
-                                <div className="flex flex-col text-primary-dark-grey">
-                                    <div className="text-[16px] font-bold text-primary-blue">
-                                        {summary.content.title}
-                                    </div>
-                                    <div className="font-bold">
-                                        {summary.content.sender || "Sender"} :
-                                    </div>
-                                    <div className="grid">
-                                        <div className="truncate">
-                                            {summary.content.message}
+                                        onClick={() => setChatDetailId(summary.id)}>
+                                        <div className="flex flex-row min-w-[50px] overflow-hidden mr-[10px]">
+                                            {
+                                                summary.participants.map((user, index) => {
+                                                    return (
+                                                        <div key={user.name + index} className="w-[15px] overflow-visible" title={user.name}>
+                                                            <div className={`flex items-center justify-center h-[30px] w-[30px] rounded-full ${user.styleClass || "bg-[#ffffff]"}`}>
+                                                                <img className="w-[12px] h-[12px]" src={user.icon}></img>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
                                         </div>
+                                        <div className="flex flex-col text-primary-dark-grey">
+                                            <div className="text-[16px] font-bold text-primary-blue">
+                                                {summary.content.title}
+                                            </div>
+                                            <div className="font-bold">
+                                                {summary.content.sender || "Sender"} :
+                                            </div>
+                                            <div className="grid">
+                                                <div className="truncate">
+                                                    {summary.content.message}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="min-w-[145px] ml-[20px]">{summary.content.dateTime}</div>
                                     </div>
-                                </div>
-                                <div className="min-w-[145px] ml-[20px]">{summary.content.dateTime}</div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
+                                )
+                            })
+                        }
+                    </div>
+            }
         </>
     );
 }

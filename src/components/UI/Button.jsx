fd39283d9b7e2245d912139199b2c2e1a3548120
Ilham Burhanuddin <ilham.r@wistkey.dev>
2024-08-14 22:05:58 +0700

    debug button

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,6 +1,6 @@
-function Button({type="button", addClass, children}) {
+function Button({type="button", addClass, onClick, children}) {
     return (
-        <button type={type} className={`px-[16px] py-[8px] rounded-[5px] bg-primary-blue text-[#FFFFFF] ${addClass}`}>
+        <button type={type} onClick={onClick} className={`px-[16px] py-[8px] rounded-[5px] bg-primary-blue text-[#FFFFFF] ${addClass}`}>
             {children}
         </button>
     );

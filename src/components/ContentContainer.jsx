73ef6f5af45b4a3c00d14737058548306dbc035f
Ilham Burhanuddin <ilham.r@wistkey.dev>
2024-08-27 10:12:47 +0700

    add max w to container

--- src/components/ContentContainer.jsx
+++ src/components/ContentContainer.jsx
@@ -1,7 +1,7 @@
 function ContentContainer({ children }) {
     return (
         <>
-            <div className="flex flex-col fixed bottom-[100px] right-[15px] rounded-[5px] bg-[#ffffff] px-[32px] py-[24px] h-[calc(100vh-115px)] w-[734px]">{children}</div>
+            <div className="flex flex-col fixed bottom-[100px] right-[15px] rounded-[5px] bg-[#ffffff] px-[32px] py-[24px] h-[calc(100vh-115px)] w-[734px] max-w-[calc(100%-30px)]">{children}</div>
         </>
     );
 }
